1f42d0d139124c5e8be6ae0878844c94a7a711df
northNomad <alexch.liu@mail.utoronto.ca>
2022-05-11 18:00:18 -0400

    Now annotates raw data with gating info

--- R/ggGate.R
+++ R/ggGate.R
@@ -1,10 +1,14 @@
 #' Create gates from ggplot2 object
 #'
-#' Create gates by clicking on ggplot2
+#' Create gates by clicking on ggplot2.
+#' A single click adds coordinates to the active polygon gate.
+#' A double click draws the active polygon gate, and appends the raw ggplot2 data with new columns.
+#' The new columns are named after the gates and contain binary values of whether each row falls into the gate.
 #'
 #' @param p ggplot2 object.
 #' @param write_gate_to String. This is the variable name of the \code{data.frame} that stores the gating coordinates.
 ggGate <- function(p,
+                   write_data_to = "df_new",
                    write_gate_to = "df_gate") {
   #
   require(shiny)
@@ -21,14 +25,25 @@ ggGate <- function(p,
                  click = "plot_click",
                  dblclick = "plot_dblclick"),
 
-      #DISPLAYS GATE COORDINATES
-      tableOutput("df_coordinate"),
-
       #SAVE COORDINATES
-      actionButton('save_to_global', "Save coordinates to global environment")
+      actionButton('save_coord_to_global', "Save coordinates to global environment"),
+
+      #SAVE APPENDED RAW DATA
+      actionButton('save_annotated.raw_to_global', "Save annotated raw data to global environment"),
+
+      #DISPLAYS GATE COORDINATES
+      tableOutput("df_coordinate")
+
+
     ),
     ####################
     server = function(input, output) {
+      #EXTRACT X, Y COLUMNS IN PLOT
+      point.x <- dplyr::select(p$data, as_label(p$mapping$x)) %>% .[, 1]
+      point.y <- dplyr::select(p$data, as_label(p$mapping$y)) %>% .[, 1]
+
+      #MAKE RAW DATA REACTIVE
+      raw <- reactiveVal(p$data)
 
       #RENDERS IMPORTED GGPLOT2 OBJECT
       p <- reactiveVal(p)
@@ -51,21 +66,39 @@ ggGate <- function(p,
         }
       })
 
-      #DOUBLECLICK
+      #DOUBLECLICK -
       observeEvent(input$plot_dblclick, {
+        ##ADDS POLYGON
         p(p() +
             geom_polygon(data = df(),
                          aes(x, y, fill = group),
                          alpha = .2)
+        )
+
+        ##ANNOTATES RAW DF
+        pol.x <- subset(df(), group == input$name_polygon) %>% .$x
+        pol.y <- subset(df(), group == input$name_polygon) %>% .$y
+        is.gated <- sp::point.in.polygon(point.x, point.y, pol.x, pol.y)
+        #
+        raw(
+          raw() %>%
+            mutate(!! input$name_polygon := is.gated)
+
         )
       })
 
       #RENDERS GATE COORDINATES AS TABLE
       output$df_coordinate <- renderTable({df()})
 
+      #WRITE COORDINATES OF GATES TO GLOBAL ENV
+      observeEvent(input$save_coord_to_global, {
+        assign(write_gate_to, df(), envir = .GlobalEnv)
+        })
 
-      observeEvent(input$save_to_global, {
-        assign(write_gate_to, df(), envir = .GlobalEnv)}
-      )}
+      #WRITE ANNOTATED RAW DATA.FRAME TO GLOBAL ENV
+      observeEvent(input$save_annotated.raw_to_global, {
+        assign(write_data_to, raw(), envir = .GlobalEnv)
+        })
+    }
   )
 }
